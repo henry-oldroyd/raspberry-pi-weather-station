0cd6eca6df45ef3cd5529c5d70a5e10a779c6c25
gangel27 <georgeangeluk@gmail.com>
2022-01-31 21:27:59 +0000

    Put back OG js, and changed HTML, still css troubles

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,35 +1,92 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
+let initialData = [28,29,31];
+var x = 4;
+var chart;
+var running = false;
+var interval;
+var data;
+
+function createChart(intialData,ctx) {
+    var myChart = new Chart(ctx, {
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Day 1', 'Day2', 'Day 3'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Temperature',
+            data: initialData,
+            backgroundColor: 'rgba(255,0,0,1)',
+            color: 'rgba(255,0,0,1)',
+            borderColor: 'rgba(255,0,0,0.2)'
+
         }]
     },
     options: {
         scales: {
             y: {
                 beginAtZero: true
-            }
+                }
+            },
+        responsive: false,
+        interaction: {
+            intersection: false,
         }
-    }
-});
+        }
+    });
+    return myChart;
+}
+
+function updateChart(data) {
+    newData = Math.floor((Math.random() +2)* 10);
+
+    chart.data.labels.push("Day " + x);
+    x = ++x;
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(newData);
+    });
+    chart.update();
+
+}
+
+
+window.addEventListener('load', function(){
+    var ctx = document.getElementById('myChart').getContext('2d');
+    chart = createChart(initialData,ctx);
+
+    var addDataButton = document.getElementById("oneData");
+    addDataButton.addEventListener("click", function(){updateChart("data goes here")})
+
+    var contDataButton = document.getElementById("contData");
+    contDataButton.addEventListener('click', function(){
+        if (running == false) {
+            running = true;
+            interval = setInterval(function(){
+                updateChart("data goes here")
+            },100)
+        }
+
+    var stopDataButton = document.getElementById('stop');
+    stopDataButton.addEventListener("click", function(){
+        clearInterval(interval);
+        running = false;
+    });
+
+    })
+
+})
+
+// function readTextFile(file, callback) {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function() {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
+// function getData() {
+//     readTextFile("test_data.json", function(text){
+//     var data = JSON.parse(text);
+//     console.log(data);
+//     return data
+// })
+// }
