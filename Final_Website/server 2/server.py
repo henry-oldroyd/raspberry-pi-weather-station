325ba87c6c906c3e0ec751faa9a2eb2167101646
henry-oldroyd <henryoldroyd10@gmail.com>
2022-05-31 13:34:03 +0100

    edits to pi emulator and server 2

--- Final_Website/server 2/server.py
+++ Final_Website/server 2/server.py
@@ -1,12 +1,17 @@
-from flask import Flask, redirect, request, jsonify, url_for
+# from flask import Flask, redirect, request, jsonify, url_for
+import hashlib
+import flask
+import flask_marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 from datetime import datetime
 
+from pyparsing import ParseSyntaxException
+
 
 # initialing our flask app, SQLAlchemy and Marshmallow
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/datastore.db'
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -17,6 +22,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+with open('hashed_key.key', 'r') as file:
+    SECRET_KEY_HASH = file.read()
+PORT = 5000
+# logger.info(f"fetched secret key hash: {SECRET_KEY_HASH}")
+# logger.info(f"Using port {PORT}")
 
 
 #this is our database model
@@ -46,41 +58,44 @@ class Data_Reading_Schema(ma.Schema):
         # https://stackoverflow.com/questions/53606872/datetime-format-in-flask-marshmallow-schema
         datetimeformat = '%Y-%m-%d %H:%M:%S'
 
+    @flask_marshmallow.pre_load
+    def add_timestamp(self, data, **kwargs):
+        data['timestamp'] = datetime.now()
+        return data
 
 data_reading_schema = Data_Reading_Schema()
 data_reading_schema_many = Data_Reading_Schema(many=True)
 
 
+
+
+def hash(plain_txt):
+    """one way hash using sha256"""
+    hash_ = hashlib.sha256()
+    hash_.update(plain_txt.encode())
+    return hash_.hexdigest()
+
+
 #adding a post
-@app.route('/add_data', methods=['POST'])
+@app.route('/add_reading', methods=['POST'])
 def add_post():
-    data_header = request.json
+    data_header = flask.request.json
     print("server received data")
     print(data_header)
-    
-    pressure = data_header['pressure']
-    temperature = data_header['temperature']
-    humidity = data_header['humidity']
-    wind_speed = data_header['wind_speed']
-    wind_direction = data_header['wind_direction']
-    precipitation = data_header['precipitation']
-    timestamp = datetime.now()
-    
 
-    new_data_reading = Data_Reading(
-        pressure=pressure, 
-        temperature=temperature,
-        humidity=humidity,
-        wind_speed=wind_speed,
-        wind_direction=wind_direction,
-        precipitation=precipitation,
-        timestamp=timestamp
-    )
+    secret_key = data_header["secret_key"]
+    new_reading_data: dict = data_header["new_data_item"]
+    
+    if hash(secret_key) != SECRET_KEY_HASH:
+        print("secret key wrong for post request")
+        flask.abort(401)
+    
+    new_reading_obj = data_reading_schema.load(new_reading_data)
 
-    db.session.add(new_data_reading)
+    db.session.add(new_reading_obj)
     db.session.commit()
 
-    return data_reading_schema.jsonify(new_data_reading)
+    return data_reading_schema.jsonify(new_reading_obj)
 
 
 #getting posts
@@ -89,11 +104,11 @@ def get_data():
     all_posts = Data_Reading.query.all()
     result = data_reading_schema_many.dump(all_posts)
 
-    return jsonify(result)
+    return flask.jsonify(result)
 
 @app.route("/", methods=['GET'])
 def index():
-    return redirect(url_for("get_data"))
+    return flask.redirect(flask.url_for("get_data"))
 
 # #getting particular post
 # @app.route('/post_details/<id>/', methods=['GET'])
@@ -130,4 +145,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=PORT, debug=True)
