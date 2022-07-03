3d6b0cb2f0b9110c89796c835770b90d986d444d
henry-oldroyd <henryoldroyd10@gmail.com>
2022-07-03 16:27:52 +0100

    updates

--- Finished website/server/server.py
+++ Finished website/server/server.py
@@ -66,7 +66,7 @@ class Reading(Base):
     temperature = sqla.Column(sqla.Float())
     humidity = sqla.Column(sqla.Float())
     wind_speed = sqla.Column(sqla.Float())
-    wind_direction = sqla.Column(sqla.Float())
+    wind_direction = sqla.Column(sqla.Integer())
     precipitation = sqla.Column(sqla.Float())
 
     def __init__(self, pressure, temperature, humidity, wind_speed, wind_direction, precipitation):
