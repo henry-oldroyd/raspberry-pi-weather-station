4b1e544f340aa2e0a3056aeaab8097458bd34f2a
henry-oldroyd <henryoldroyd10@gmail.com>
2022-07-04 19:48:03 +0100

    pi update logging

--- Finished website/pi_emulator/pi_emulator.py
+++ Finished website/pi_emulator/pi_emulator.py
@@ -1,14 +1,26 @@
 import requests
 from random import uniform, randint
+import os
 
+# logging
+import logging
+import logger as logger_module
 
 SERVER_URL = 'http://127.0.0.1:5000/data'
 
 
-# will be stored in an environmental variable in PI
+# setup logger
+logger_module.setup_logger(os.path.basename(__file__))
+logger = logging.getLogger(os.path.basename(__file__))
+
+
 with open("secret_key.key", "r") as file:
     SECRET_KEY = file.read()
 
+
+output_safe_secret_key = SECRET_KEY[:4] + "*"*(len(SECRET_KEY)-8) + SECRET_KEY[-4:]
+logger.info(f"using secret key:   {output_safe_secret_key}")
+logger.info(f"using server url:   {SERVER_URL}")
 
 def send_reading(pressure, temperature, humidity, wind_speed, wind_direction, precipitation):
     reading_data = {
@@ -19,20 +31,24 @@ def send_reading(pressure, temperature, humidity, wind_speed, wind_direction, pr
         'wind_direction': wind_direction,
         'precipitation': precipitation
     }
-
+    logger.info(f"data about to be sent in post request:   {reading_data}")
 
     payload = {"secret_key": SECRET_KEY, "new_data_item": reading_data}
 
     try:
         request = requests.post(SERVER_URL, json=payload)
-        # request = requests.post('https://stackoverflow.com/', json=data_to_send)
         assert request.status_code == 200, f"Status code was not 200 success but was  {request.status_code}"
     except Exception as e:
         print("ERROR:")
         print(e)
+        if request.status_code == 404:
+                logger.critical('404 page not found')
+        if request.status_code == 401:
+                logger.critical('401 authentication failed, check key')
+        if request.status_code == 500:
+                logger.critical('500 internal server error, perhaps flask server has crashed')
 
 if __name__ == '__main__':
-
     send_reading(
         pressure=uniform(0, 30),
         temperature=uniform(0, 30),
