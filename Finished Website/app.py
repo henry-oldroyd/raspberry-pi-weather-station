d2650e71dba5efd49d5347ecf6d6396dfa806267
Devayan-Patel <98704099+Devayan-Patel@users.noreply.github.com>
2022-07-17 11:00:36 +0100

    Removed waitress from app.py
    Done as the Flask application will now use gunicorn instead of waitress as its WSGI server

--- Finished Website/app.py
+++ Finished Website/app.py
@@ -1,7 +1,5 @@
-from waitress import serve
 from server import server
 
 FLASK_APP = server.app
 print("http://127.0.0.1:5000/")
-# serve(FLASK_APP, host='0.0.0.0', port=8080)
-serve(FLASK_APP, host='127.0.0.1', port=5000)
+FLASK_APP.run(host='127.0.0.1', port=5000)
