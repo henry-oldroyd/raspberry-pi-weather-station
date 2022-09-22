d02ad060b21b0b593f9ce09e0e01d1bad0431ecf
henry-oldroyd <henryoldroyd10@gmail.com>
2022-09-22 19:35:19 +0100

    dealt with warning when running

--- Finished Website/app.py
+++ Finished Website/app.py
@@ -1,5 +1,7 @@
 from server import server
 
 FLASK_APP = server.app
-print("http://127.0.0.1:5000/")
-FLASK_APP.run(host='127.0.0.1', port=5000)
+
+if __name__ == '__main__':
+    print("http://127.0.0.1:5000/")
+    FLASK_APP.run(host='127.0.0.1', port=5000)
